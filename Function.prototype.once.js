bd8ba86a2ac667cf3c1ddbf71f2961a0f02a0915
zzzzBov <zzzzBov@gmail.com>
2012-08-13 16:53:14 -0400

    Created `Function.prototype.once.js` which contains a function to generate functions that can only be called `n` times.
    When `onceFn = fn.once(n)` is called, it returns a function
    that will only execute the first `n` times it's called using
    the parameters provided to `onceFn`. If no parameter is
    provided, `n = 1`.
    
    If `onceFn` can still be called, the return value of `onceFn`
    is the return value of `fn`, otherwise the second, optional,
    `ret` parameter is used as the return value.

---
+++ Function.prototype.once.js
@@ -0,0 +1,27 @@
+(function (slice) {
+    "use strict";
+    if (!Function.prototype.once) {
+        Function.prototype.once = function (n, ret) {
+            var fn;
+            fn = this;
+            if (typeof fn !== 'function') {
+                throw new Error('Function.prototype.once must be called on a function.');
+            }
+            if (!arguments.length) {
+                n = 1;
+            }
+            n = +n;
+            if (isNaN(n) || n < 1) {
+                throw new Error('Function.prototype.once requires a positive number as a parameter.');
+            }
+            return function () {
+                var args;
+                args = slice.call(arguments);
+                if (n-- > 0) {
+                    return fn.apply(this, args);
+                }
+                return ret;
+            };
+        };
+    }
+}(Array.prototype.slice));
