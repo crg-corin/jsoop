eef42d4b2538a69df57dd39b959a605334d92cf7
zzzzBov <zzzzBov@gmail.com>
2012-08-15 17:23:56 -0400

    Significantly improved decodeHTML to include HTML entities.

--- String.prototype.decodeHTML.js
+++ String.prototype.decodeHTML.js
@@ -2,6 +2,7 @@
  * @dependencies
  * String
  * String.prototype.replace
+ * String.fromCharCode
  * TypeError
  * 
  * @dependents
@@ -11,20 +12,37 @@
  * @return String
  * returns the html decoded string
  */
-(function () {
+(function (charCode, undefined) {
     "use strict";
+    var entities,
+        entityPattern;
+    function entityRepresentation(match, hex, num, entity) {
+        if (hex !== undefined) {
+            num = parseInt(hex, 16);
+            return charCode(num);
+        } else if (num !== undefined) {
+            return charCode(num);
+        } else if (entity !== undefined && entities.hasOwnProperty(entity)) {
+            return entities[entity];
+        }
+        return match;
+    }
+    entityPattern = /&(?:#x([0-9a-f]+)|#(\d+)|([0-9a-z]+))(?:;|(?=&))/gi;
+    entities = {
+        'amp': '&',
+        'quot': '"',
+        'lt': '<',
+        'gt': '>',
+        'apos': '\''
+        //add more another time
+    };
+    
     if (!String.prototype.decodeHTML) {
         String.prototype.decodeHTML = function () {
             if (this === null || this === undefined) {
-                throw new TypeError('this is null or not defined');
+                throw new TypeError('"this" is null or not defined');
             }
-            return String(this)
-                .replace(/&#x2F;/g, '/')
-                .replace(/&#x27;/g, "'")
-                .replace(/&quot;/g, '"')
-                .replace(/&gt;/g, '>')
-                .replace(/&lt;/g, '<')
-                .replace(/&amp;/g, '&');
+            return String(this).replace(entityPattern, entityRepresentation);
         };
     }
-}());
+}(String.fromCharCode));
