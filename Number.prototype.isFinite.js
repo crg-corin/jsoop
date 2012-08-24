46ef8a396253b64272e2fdfefe46f838ef78ddcf
zzzzBov <zzzzBov@gmail.com>
2012-08-24 11:47:33 -0400

    Store and use the window.isFinite method

--- Number.prototype.isFinite.js
+++ Number.prototype.isFinite.js
@@ -7,22 +7,11 @@
  * 
  * @return
  */
-(function () {
+(function (isFinite) {
     "use strict";
     if (!Number.prototype.isFinite) {
         Number.prototype.isFinite = function () {
-            var t;
-            if (this === null || this === undefined) {
-                throw new TypeError('this is null or not defined');
-            }
-            t = Number(this);
-            if (isNaN(t)) {
-                return false;
-            }
-            if (t === Number.POSITIVE_INFINITY || t === Number.NEGATIVE_INFINITY) {
-                return false;
-            }
-            return true;
+            return isFinite(this);
         };
     }
-}());
+}(window.isFinite));
