52404166e3bf5bc5454371e44623a73c37fd1354
zzzzBov <zzzzBov@gmail.com>
2012-09-27 10:07:48 -0400

    Updated Object.fromPath.js to include more documentation Normalized loop structure.

--- Object.fromPath.js
+++ Object.fromPath.js
@@ -2,20 +2,28 @@
     "use strict";
     if (!Object.fromPath) {
         Object.fromPath = function (context, path) {
-            var keys,
-                i,
-                l,
-                result;
-            //if called as `Object.fromPath('foo.bar.baz')`, assume `window` as context
+            var result,
+                keys,
+                i;
+            //if called as `Object.fromPath('foo.bar.baz')`,
+            //assume `window` as context
             if (arguments.length < 2) {
                 path = context;
                 context = window;
             }
+            //start at the `context` object
             result = context;
+            //break the path on `.` characters
             keys = String(path).split('.');
-            for (i = 0, l = keys.length; i < l && result != null; i++) {
+            //`!= null` is being used to break out of the loop
+            //if `null` or `undefined are found
+            for (i = 0; i < keys.length && result != null; i+= 1) {
+                //iterate down the path, getting the next part
+                //of the path each iteration
                 result = result[keys[i]];
             }
+            //return the object as described by the path,
+            //or null or undefined if they occur anywhere in the path
             return result;
         };
     }
