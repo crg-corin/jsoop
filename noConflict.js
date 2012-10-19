a816638471d5b87d9d47ad32e3db86999cb15ea6
zzzzBov <zzzzBov@gmail.com>
2012-10-19 12:28:40 -0400

    Updated noConflict.js to support 1-3 parameters

--- noConflict.js
+++ noConflict.js
@@ -4,42 +4,60 @@
     //noConflict('prop', {obj}); //call noConflict(base, 'prop', obj); using window
     //noConflict({base}, 'prop', {obj});
     //noConflict([bases], [props], {obj});
-    if (!window.noConflict) {
-        window.noConflict = function (base, prop, obj) {
-            var deleteProp,
-                backup;
-            if (arguments.length === 2) {
-                obj = prop;
-                prop = base;
-                base = window;
+    
+    function noConflict(base, prop, obj) {
+        var deleteProp,
+            backup,
+            i;
+        switch (arguments.length) {
+        case 0: //noConflict();
+            throw new Error('At least one parameter must be provided');
+        case 1: //noConflict({map});
+            return noConflict(window, base);
+        case 2: //noConflict('prop', {obj});
+                //noConflict({base}, {map});
+            if (typeof base === 'string') {
+                return noConflict(window, base, prop);
             }
-            if (base == null /* or undefined */) {
-                throw new Error('"base" must be an object.');
+            if (prop == null /* or undefined */) {
+                throw new Error('"map" must be an object.');
             }
-            //prop must be a string
-            prop = '' + prop;
-            if (obj == null /* or undefined */) {
-                throw new Error('"obj" must be an object.');
-            }
-            
-            if (base.hasOwnProperty(prop)) {
-                deleteProp = false;
-                backup = base[prop];
-            } else {
-                deleteProp = true;
-            }
-            
-            base[prop] = obj;
-            
-            obj.noConflict = function () {
-                if (deleteProp) {
-                    delete base[prop];
-                } else {
-                    base[prop] = backup;
+            for (i in prop) {
+                if (prop.hasOwnProperty(i)) {
+                    noConflict(base, i, prop[i]);
                 }
-                delete obj.noConflict;
-                return this;
-            };
+            }
+            return;
+        }
+        if (base == null /* or undefined */) {
+            throw new Error('"base" must be an object.');
+        }
+        prop = '' + prop;
+        if (obj == null /* or undefined */) {
+            throw new Error('"obj" must be an object.');
+        }
+        
+        if (base.hasOwnProperty(prop)) {
+            deleteProp = false;
+            backup = base[prop];
+        } else {
+            deleteProp = true;
+        }
+        
+        base[prop] = obj;
+        
+        obj.noConflict = function () {
+            if (deleteProp) {
+                delete base[prop];
+            } else {
+                base[prop] = backup;
+            }
+            delete obj.noConflict();
+            return this;
         };
+    }
+    
+    if (!window.noConflict) {
+        window.noConflict = noConflict;
     }
 }());
