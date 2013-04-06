5e68c9231bcbde97c3fcb2ad2c983d1df97c2598
Timothy <zzzzBov@gmail.com>
2013-04-06 17:26:37 -0400

    Started QueryString.js

--- jsoop/querystring.js
+++ jsoop/querystring.js
@@ -1,1 +1,12 @@
 ///QueryString.js
+///QueryString is a multiton
+
+(function (root, undefined) {
+    "use strict";
+    
+    if (!root.QueryString) {
+        root.QueryString = function () {
+            
+        };
+    }
+}(this));
