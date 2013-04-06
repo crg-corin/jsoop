6c38262ea59369fb743037f28279a5f09bc604c8
Timothy <zzzzBov@gmail.com>
2013-04-06 17:35:46 -0400

    added some notes in QueryString.js

--- jsoop/querystring.js
+++ jsoop/querystring.js
@@ -1,6 +1,13 @@
 ///QueryString.js
 ///QueryString is a multiton
 
+
+/**
+ * '' is the implied null query string
+ * '?' is the empty query string
+ */
+
+
 (function (root, undefined) {
     "use strict";
     
